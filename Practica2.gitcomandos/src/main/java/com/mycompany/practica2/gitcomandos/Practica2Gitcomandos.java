d8ae596e1394f0fd98891f6271a0dfa5cce98feb
franc <franc@Lacria>
2025-11-20 21:27:19 +0100

    Calculadora Practica 2

---
+++ Practica2.gitcomandos/src/main/java/com/mycompany/practica2/gitcomandos/Practica2Gitcomandos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica2.gitcomandos;
+
+/**
+ *
+ * @author franc
+ */
+public class Practica2Gitcomandos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
